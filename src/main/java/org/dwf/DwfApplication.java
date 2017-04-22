97bec06e7000305d40e3d8fb0be5079325394782
Charlie Winnard <ctwinnard@gmail.com>
2017-04-22 16:10:15 -0400

    second commit

---
+++ src/main/java/org/dwf/DwfApplication.java
@@ -0,0 +1,12 @@
+package org.dwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwfApplication.class, args);
+	}
+}
